3c171bfc68f8ef7042571d539728c18743a83d1c
iidakyouhei <kunekune0803@gmail.com>
2020-12-26 20:49:33 +0900

    Delete-update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,4 +18,22 @@ class TasksController < ApplicationController
     @task   = Task.find(id)
     @status = ['todo', 'doing', 'done']
   end
+
+  def update
+    id   = params[:id]
+    task = Task.find(1)
+
+    task.task         = params[:task]
+    task.state        = params[:state]
+    task.limit_date   = params[:limit_date]
+    task.save
+
+    redirect_to '/tasks', notice: 'タスクを更新しました。'
+  end
+  
+  def destroy
+    task       = Task.find(params[:id])
+    task.destroy
+    redirect_to '/tasks', notice: 'タスクを削除しました。'
+  end
 end
