af5ea6405acf53107f7e15149b1342eab4317a30
iidakyouhei <kunekune0803@gmail.com>
2020-12-26 20:23:03 +0900

    registration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,19 @@ class TasksController < ApplicationController
     @tasks  = Task.order('limit_date').all
     @status = ['todo', 'doing', 'done']  
   end
+
+  def store
+    task = Task.new
+    task.task       = params[:task]
+    task.state      = params[:state]
+    task.limit_date = params[:limit_date]
+    task.save
+    redirect_to '/tasks', notice: 'タスクを作成しました。'
+  end
+  
+  def show
+    id      = params[:id]
+    @task   = Task.find(id)
+    @status = ['todo', 'doing', 'done']
+  end
 end
